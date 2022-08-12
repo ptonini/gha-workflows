16eadb01d1056cc175479ebf5e7ea593ed133705
Pedro Tonini <pedro.tonini@hotmail.com>
2022-08-12 19:22:43 -0300

    added project files

---
+++ main.tf
@@ -0,0 +1,28 @@
+variable "github_token" {}
+
+locals {
+  github_owner  = "nodis-com-br"
+  main_workflow = ".github/workflows/main-workflow.yaml"
+}
+
+provider "github" {
+  owner = local.github_owner
+  token = var.github_token
+}
+
+module "k8s_workflow_deploy_helm_release" {
+  source  = "git@github.com:nodis-com-br/tf_modules.git//github_repository_file"
+  topics  = ["helm-release github-flow"]
+  file    = ".github/workflows/deploy.yml"
+  content = file("src/deploy-helm-release.yml")
+  owner   = local.github_owner
+}
+
+module "k8s_workflow_configure_helm_release" {
+  source  = "git@github.com:nodis-com-br/tf_modules.git//github_repository_file"
+  topics  = ["helm-release github-flow"]
+  file    = ".github/workflows/configure.yml"
+  content = file("src/configure-helm-release.yml")
+  owner   = local.github_owner
+}
+
