165e1ff5a6766cdfe3a870073895a203b16eaf80
Pedro Tonini <pedro.tonini@hotmail.com>
2022-09-23 23:04:22 -0300

    added publish go workflow

--- main.tf
+++ main.tf
@@ -26,3 +26,11 @@ module "k8s_workflow_configure_helm_release" {
   owner   = local.github_owner
 }
 
+module "workflow_publish_go_application" {
+  source  = "git@github.com:nodis-com-br/tf_modules.git//github_repository_file"
+  topics  = ["vault-plugin github-flow"]
+  file    = ".github/workflows/publish.yml"
+  content = file("src/publish-go-application.yml")
+  owner   = local.github_owner
+}
+
