13f738528bc1e5fb7757d114f46a5f6443b3e397
ptonini <pedro.tonini@hotmail.com>
2023-05-03 13:49:24 -0300

    chore: added project files

---
+++ .terraform.lock.hcl
@@ -0,0 +1,23 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/integrations/github" {
+  version = "5.24.0"
+  hashes = [
+    "h1:bjT9YMjGtOvL4FWxthpEoS2alp52X/USvd9vUPEe8d0=",
+    "zh:01c544600c48a7ba5822c8ddf717eeb580360c3b033268bf905c5489fb4bb337",
+    "zh:14f6ceae2234d75a539df7ce3223b77eddce48f848ffbffd87b5a81027afadf8",
+    "zh:18effbc9957a804a439ad742c3475510ab8349548255e6f5096160154aac23dc",
+    "zh:1f75ce3ff800d61d148f3a95d802ecf45f9386d6e2ebfe58c224914377a95741",
+    "zh:2a333220d04ab2e3278a252cea84ca34f7e84b7362bb285673a4d132c037b238",
+    "zh:4fb2a338d84d656ba890469246973faaa70b6c559c61f12c19f50fe99b7749f7",
+    "zh:5b2bb8281e2237e2b682833ac1d19f002ac9a4c036ff32c3ba0c8e62fe3e4292",
+    "zh:7748493f26f1a99df13c7e3ba8d82d9cf200c168314ce3467c32214180118df2",
+    "zh:91ebc65f4236c5810a70718751381e2909a68e573e264a36369658b95477e542",
+    "zh:d1d476f935b95ecafa8b6579eaeabe44964f14ac16a9e1b58fe28797a4d10d2b",
+    "zh:d38045d07fccd87a64fe480e750d1e004ff6ae8aedb89fd3310ba13e9b5ee6bf",
+    "zh:d46edbdaf3f5b5e34c831bf7a4ad11c267ec4d079ca9f907d8f97f33073a40eb",
+    "zh:eda90ef78f94728d3ce81e8b479cbf23fc247b5764b496d32c54d249ca537270",
+    "zh:f7d551125d7fa99572960877e2f3edc796dc3b04c9024e5c7e7ed5a764a8d4d8",
+  ]
+}
--- main.tf
+++ main.tf
@@ -9,18 +9,113 @@ provider "github" {
   token = var.github_token
 }
 
-module "workflow_publish_helm_monorepo" {
-  source  = "./modules/github_repository_file"
-  topics  = ["helm-charts github-flow"]
-  file    = ".github/workflows/publish.yml"
-  content = file("src/publish-charts.yml")
+
+# Github flow
+
+module "repo_group_github_flow" {
+  source  = "ptonini/repository-group/github"
+  version = "~> 1.0.0"
+  topics  = ["github-flow"]
   owner   = local.owner
 }
 
-module "workflow_publish_docker_monorepo" {
-  source  = "./modules/github_repository_file"
-  topics  = ["container-images github-flow"]
-  file    = ".github/workflows/publish.yml"
-  content = file("src/publish-docker-monorepo.yml")
+resource "github_actions_variable" "container_registry" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "container_registry"
+  value         = "ghcr.io/${local.owner}"
+}
+
+resource "github_actions_variable" "rp_manifest_file" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "rp_manifest_file"
+  value         = ".release-please-manifest.json"
+}
+
+resource "github_actions_variable" "go_version" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "go_version"
+  value         = "1.18"
+}
+
+resource "github_actions_variable" "python_version" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "python_version"
+  value         = "3.9.12"
+}
+
+resource "github_actions_variable" "nodejs_version" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "nodejs_version"
+  value         = "16.10.0"
+}
+
+resource "github_actions_variable" "lua_version" {
+  for_each      = module.repo_group_github_flow.this
+  repository    = each.key
+  variable_name = "lua_version"
+  value         = "5.4.4"
+}
+
+
+# Helm charts
+
+module "workflow_publish_helm_charts" {
+  source     = "ptonini/repository-file/github"
+  version    = "~> 1.0.0"
+  repository = { name = "helm-charts", default_branch = "main" }
+  file       = ".github/workflows/publish-monorepo.yml"
+  content    = file("src/publish-charts-monorepo.yml")
+}
+
+
+# Luarocks
+
+module "workflow_publish_luarocks" {
+  source     = "ptonini/repository-file/github"
+  version    = "~> 1.0.0"
+  repository = { name = "luarocks", default_branch = "main" }
+  file       = ".github/workflows/publish-monorepo.yml"
+  content    = file("src/publish-luarocks-monorepo.yml")
+}
+
+
+# Container images (monorepo)
+
+module "workflow_publish_container_images" {
+  source     = "ptonini/repository-file/github"
+  version    = "~> 1.0.0"
+  repository = { name = "container-images", default_branch = "main" }
+  file       = ".github/workflows/publish-monorepo.yml"
+  content    = file("src/publish-docker-monorepo.yml")
+}
+
+
+# Container image
+
+module "repo_group_container_image" {
+  source  = "ptonini/repository-group/github"
+  version = "~> 1.0.0"
+  topics  = ["container-image github-flow"]
   owner   = local.owner
+}
+
+module "workflow_publish_container_image" {
+  source     = "ptonini/repository-file/github"
+  version    = "~> 1.0.0"
+  for_each   = module.repo_group_container_image.this
+  repository = each.value
+  file       = ".github/workflows/publish.yml"
+  content    = file("src/publish-docker.yml")
+}
+
+resource "github_actions_variable" "pr_approved_tag" {
+  for_each      = module.repo_group_container_image.this
+  repository    = each.key
+  variable_name = "pr_approved_tag"
+  value         = "pr-approved"
 }
